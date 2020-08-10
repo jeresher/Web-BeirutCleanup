cc00f0a921fb37275eb4abc652ed83421c076949
Jere Sher <jeresher@g.ucla.edu>
2020-08-10 15:52:45 -0700

    Temporary db submit testing

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,43 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const { countReset } = require('console');
 const app = express();
 
-app.use(express.json());
+mongoose.connect('mongodb://localhost/beirut')
+.then(() => console.log("Connection Successful."))
 
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send('hey');
 });
+
+
+var postSchema = new mongoose.Schema({
+    eventName: String,
+    eventDate: {type: Date, default: Date.now},
+    eventLocationDescription: String,
+    eventDescription: String,
+    eventLongLat: [ Number ], // [ Longtitude, Latitude ]
+    eventComments: [ String ]
+});
+
+var Post = mongoose.model('Post', postSchema);
+
+async function createPost() {
+    const post = new Post({
+        eventName: "Help",
+        eventLocationDescription: "I need help here",
+        eventDescription: "This is what I need help with",
+        eventLongLat: [10, -10],
+        eventComments: ["hey bro", "cool man"]
+    })
+    
+    const result = await post.save();
+    console.log(result);
+}
+
+createPost();
 
 
 app.listen(3000);
