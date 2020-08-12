c03458ce33b0efd54cd522213ea2feabf7c2372c
Jere Sher <jeresher@g.ucla.edu>
2020-08-11 20:48:52 -0700

    CORS Access-Control-Allow-Origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,13 @@ const app = express();
 mongoose.connect('mongodb://localhost/beirut')
 .then(() => console.log("Connection Successful."))
 
+//Temporary for CORS.
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 app.use(express.json());
 app.use(routes);
