eb1add8790eae532c0eb668ecc794fb533b5b97e
Jere Sher <jeresher@g.ucla.edu>
2020-08-12 18:45:52 -0700

    added logo

--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import logo from '../Assets/logo.png';
 
 function NavBar() {
     return(
       <div className="navbar">
-          <h1 className="logo">Beirut Cleanup</h1>
+        <div className="logo">
+          <img src={logo}></img>
+        </div>
+        <div>
+          <h1></h1>
+        </div>
+        <div>
+          <h1></h1>
+        </div>
       </div>
     );
   }
