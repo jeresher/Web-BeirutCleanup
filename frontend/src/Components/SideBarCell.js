70ded5f2f1b198ade6cc72e237783a8fe065ba8c
Jere Sher <jeresher@g.ucla.edu>
2020-08-13 08:07:36 -0700

    Added text truncation fade effect

--- frontend/src/Components/SideBarCell.js
+++ frontend/src/Components/SideBarCell.js
@@ -4,11 +4,12 @@ function SideBarCell(props) {
 
     return (
         <div className='cell'>
+            <div className="texttruncationfadeeffect"></div>
             <div className="eventtext">
-                <h4>TODAY</h4>
+                <h4>TODAY | اليوم</h4>
                 <h1>Red Cross Cleanup Event</h1>
                 <div className="miniLine" />
-                <p>Hi there. I'm organizing a cleanup activity today. I need everyone to be there because it's going to be extra lit. please be there. otherwise I'm going to be pretty damn sad. You don't want to see that. trust.</p>
+                <p>Hi there. I'm organizing a cleanup activity today. I need everyone to be there because it's going to be extra lit. please be there. otherwise I'm going to be pretty damn sad. You don't want to see that. trust.Hi there. I'm organizing a cleanup activity today. I need everyone to be there because it's going to be extra lit. please be there. otherwise I'm going to be pretty damn sad. You don't want to see that. trust. </p>
             </div>
             <div className="moreinformation">
                 <button>MORE INFORMATION | معلومات اكثر</button>
