107e3e1eb8b73e29f24b97a52187955e9718ad0d
Jere Sher <jeresher@g.ucla.edu>
2020-08-13 11:48:01 -0700

    Added marker properties to the cells

--- frontend/src/Components/SideBarCell.js
+++ frontend/src/Components/SideBarCell.js
@@ -15,18 +15,14 @@ function SideBarCell(props) {
             <div className="texttruncationfadeeffect"></div>
             <div className="eventtext">
                 <h4>TODAY | اليوم</h4>
-                <h1>Red Cross Cleanup Event</h1>
+                <h1>{marker.eventName}</h1>
                 <div className="miniLine" />
-                <p>Hi there. I'm organizing a cleanup activity today. I need everyone to be there because it's going to be extra lit. please be there. otherwise I'm going to be pretty damn sad. You don't want to see that. trust.Hi there. I'm organizing a cleanup activity today. I need everyone to be there because it's going to be extra lit. please be there. otherwise I'm going to be pretty damn sad. You don't want to see that. trust. </p>
+                <p>{marker.eventDescription}</p>
             </div>
             <div className="moreinformation">
                 <button>MORE INFORMATION | معلومات اكثر</button>
             </div>
-            {/*
-            <h4>{props.date}</h4>
-            <h1>{props.name}</h1>
-            <p>{props.description}</p>
-            */}
+            
         </div>
     )
 }
