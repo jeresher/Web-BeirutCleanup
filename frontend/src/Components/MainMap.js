6ff0bff8bc6d06c9af1bd0e87e911599a9539f92
Jere Sher <jeresher@g.ucla.edu>
2020-08-15 14:03:45 -0700

    Search bar working: temporary save before src restructure

--- frontend/src/Components/MainMap.js
+++ frontend/src/Components/MainMap.js
@@ -5,8 +5,20 @@ import {
   Marker,
   InfoWindow
 } from "@react-google-maps/api";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng
+} from 'use-places-autocomplete';
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption
+} from "@reach/combobox";
 import { debounce } from 'lodash';
-import mapStyles from "../Style/mapStyles"
+import "@reach/combobox/styles.css";
+import mapStyles from "../Style/mapStyles";
 
 
 const libraries = ["places"];
@@ -33,17 +45,31 @@ function MainMap(props) {
     libraries
   });
 
-  const [map, setMap] = React.useState(null)
+  const mapRef = React.useRef();
+  // const [map, setMap] = React.useState(null)
 
   const onMapLoad = React.useCallback((map) => {
-    setMap(map)
+    mapRef.current = map;
   })
 
+  /*
+  const panTo = React.useCallback(({ lat, lng }) => {
+    mapRef.current.panTo({ lat, lng });
+    mapRef.current.setZoom(14);
+  }, [])
+  */
+
+  function panTo({lat, lng}) {
+    mapRef.current.panTo({ lat, lng});
+    mapRef.current.setZoom(15);
+  };
+  // maybe use a useeffect if selected location changes, triggering panTo
+
   function onBoundsChanged() {
-    const swlng = map.getBounds().getSouthWest().lng();
-    const nelng = map.getBounds().getNorthEast().lng();
-    const swlat = map.getBounds().getSouthWest().lat();
-    const nelat = map.getBounds().getNorthEast().lat();
+    const swlng = mapRef.current.getBounds().getSouthWest().lng();
+    const nelng = mapRef.current.getBounds().getNorthEast().lng();
+    const swlat = mapRef.current.getBounds().getSouthWest().lat();
+    const nelat = mapRef.current.getBounds().getNorthEast().lat();
     props.setBounds([swlng, nelng, swlat, nelat]);
   }
 
@@ -52,6 +78,9 @@ function MainMap(props) {
 
   return (
     <div className="mainmap">
+
+      <Search panTo={panTo} />
+
       <GoogleMap 
         mapContainerStyle={mapContainerStyle} 
         zoom={13}
@@ -88,5 +117,56 @@ function MainMap(props) {
     </div>
   );
 }
+
+function Search({ panTo }) {
+  const { 
+    ready, 
+    value, 
+    suggestions: {status, data }, 
+    setValue, 
+    clearSuggestion 
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      location: {lat: () => 33.8938, lng: () => 35.5018},
+      radius: 10000 // 6 MILES.
+    }
+  })
+
+  return (
+    <div className="searchbar">
+      <Combobox 
+        onSelect={async (address) => {
+          try {
+            const results = await getGeocode({ address });
+            const { lat, lng } = await getLatLng(results[0]);
+            panTo({ lat, lng});
+          } catch(error) {
+            console.log("error!");
+          }
+        }}
+      >
+
+        <ComboboxInput
+          value={value} 
+          onChange={(event) => {
+            setValue(event.target.value)
+          }}
+          disabled={!ready}
+          placeholder="Enter an address | أدخل عنوانا"
+        />
+
+        <ComboboxPopover key="asdasd">
+          {status === "OK" && 
+          data.map(({ place_id, description }) => (
+            <ComboboxOption value={description} key={place_id} />
+          ))}
+        </ComboboxPopover>
+
+      </Combobox>
+    </div>
+  )
+}
+
+
   
 export default MainMap;
