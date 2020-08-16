bb5e6b4a97c76d72c6d83efe0a0b5bd887b45092
Jere Sher <jeresher@g.ucla.edu>
2020-08-15 19:10:10 -0700

    added donate button

--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -10,11 +10,12 @@ function NavBar(props) {
         <div>
           <h1></h1>
         </div>
-        <div className="newevent">
+        <div className="navigation">
           { props.createPage ?
-          <button onClick={() => props.loadCreatePage(false)}><h1>VIEW EVENTS</h1><h1>عرض الأحداث</h1></button> :
-          <button onClick={() => props.loadCreatePage(true)}><h1>ADD EVENT</h1><h1>إضافة حدث</h1></button>
+          <button id="viewevent" onClick={() => props.loadCreatePage(false)}><h1>VIEW EVENTS</h1><h1>عرض الأحداث</h1></button> :
+          <button id="addevent" onClick={() => props.loadCreatePage(true)}><h1>ADD EVENT</h1><h1>إضافة حدث</h1></button>
           }
+          <a href="https://supportlrc.app/" target="_blank"><button id="donate"><h1>DONATE</h1><h1>تبرع</h1></button></a>
         </div>
       </div>
     );
