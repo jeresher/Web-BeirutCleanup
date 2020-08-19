179378032675b96da58f94d2dec771225427a143
Jere Sher <jeresher@g.ucla.edu>
2020-08-19 16:28:34 -0700

    temp login commit: missing JWT implementation

--- frontend/src/Components/Admin/Login.js
+++ frontend/src/Components/Admin/Login.js
@@ -1,23 +1,50 @@
 import React from "react";
+import Config from '../../Miscellaneous/Config'
 import { Link } from "react-router-dom";
 
 function Admin() {
 
+    
+    function attemptLogin(event) {
+        event.preventDefault();
+
+        var adminEmail = document.getElementById("admin-email");
+        var adminPassword = document.getElementById("admin-password");
+
+        fetch(`${Config.url.API_URL}/api/login/`, {
+            method: "POST",
+            body: JSON.stringify({
+                "email": adminEmail.value,
+                "password": adminPassword.value
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+        .then(res => res.json())
+        .then(response => {
+            console.log(response);
+        })
+        .catch(err => console.log(err))
+        
+    }
+
 
     return (
         <div className="admin-login-main-container">
-            <div className="admin-login-inner-container">
+            <form className="admin-login-inner-container" onSubmit={attemptLogin}>
 
                 <div className="admin-login-form">
-                    <input placeholder="Email" />
-                    <input placeholder="password" type="password" />
+                    <input placeholder="email" id="admin-email" required />
+                    <input placeholder="password" type="password" id="admin-password" required />
                 </div>
 
                 <div className="admin-login-buttons">
-                    <Link to="./dashboard"><button className="submit"></button></Link>
+                    {/* <Link to="./dashboard"><input className="submit"></input></Link> */}
+                    <button type="submit" className="submit"></button>
                 </div>
                 
-            </div>
+            </form>
         </div>
     )
 
