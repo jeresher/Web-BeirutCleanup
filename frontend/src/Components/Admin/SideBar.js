62173f15527baaf3cde8c3fe00c2e837cd3320d0
Jere Sher <jeresher@g.ucla.edu>
2020-08-24 09:33:10 -0700

    added feature of sidebar adjusting depending on privileges

--- frontend/src/Components/Admin/SideBar.js
+++ frontend/src/Components/Admin/SideBar.js
@@ -4,22 +4,45 @@ import markersvg from "../../Assets/icons/marker.svg"
 import globesvg from "../../Assets/icons/globe.svg"
 
 
-function SideBar(props) {    
+function SideBar({user}) {    
 
+    function adjustSidebar() {
+
+        if (!user) return;
+        
+        if (user.privileges >= 1) {
+            const corelabel = document.getElementById('core-label');
+            const eventstab = document.getElementById('events-tab');
+            corelabel.style.display = 'block';
+            eventstab.style.display = 'block';
+        }
+
+        if (user.privileges >= 5) {
+            const adminlabel = document.getElementById('admin-label');
+            adminlabel.style.display = 'block';
+        }
+
+        if (user.privileges >= 10) {
+            const alleventstab = document.getElementById('all-events-tab');
+            alleventstab.style.display = 'block'
+        }
+    }
+
+    useEffect(adjustSidebar, [user])
 
     return (
         <div className="admin-dashboard-sidebar">
 
-            <label>CORE</label>
-            <Link to="/admin/dashboard/events" className="routerlink">
+            <label id="core-label" style={{display: "none"}}>CORE</label>
+            <Link to="/admin/dashboard/events" id="events-tab" style={{display: "none"}} className="routerlink">
             <div className="section">
                 <img src={markersvg}></img>
                 <button>Events</button>
             </div>
             </Link>
 
-            <label>ADMIN</label>
-            <Link to="/admin/dashboard/allevents" className="routerlink">
+            <label id="admin-label" style={{display: "none"}}>ADMIN</label>
+            <Link to="/admin/dashboard/allevents" id="all-events-tab" style={{display: "none"}} className="routerlink">
             <div className="section">
                 <img src={globesvg}></img>
                 <button>All Events</button>
@@ -28,7 +51,7 @@ function SideBar(props) {
 
             <div className="userinfo noselect">
                 <h5>Logged in as:</h5>
-                <h4>{props.user ? props.user.name : "Loading..."}</h4>
+                <h4>{user ? user.name : "Loading..."}</h4>
             </div>
 
         </div>
