661a1798c4a0bafca75921224f4aa15b951d916c
Jere Sher <jeresher@g.ucla.edu>
2020-08-24 15:02:51 -0700

    added intial styling for creating an account on admin panel

--- frontend/src/Components/Admin/Accounts/Main/AccountsForm.js
+++ frontend/src/Components/Admin/Accounts/Main/AccountsForm.js
@@ -2,8 +2,32 @@ import React from 'react';
 
 function AccountsForm() {
     return (
-        <div className="accounts-form">
-            Form
+        <div className="accounts-form-outer-container">
+            <div className="accounts-form-inner-container">
+                <div className="formheading">
+                    <h1>Create an Account</h1>
+                </div>
+
+                <div className="forminfo">
+                    <div className="name">
+                        <h6>Name</h6>
+                        <input placeholder="Volunteer Beirut"></input>
+                    </div>
+                    <div className="name">
+                        <h6>Email</h6>
+                        <input type="email" placeholder="example@email.com"></input>
+                    </div>
+                    <div className="name">
+                        <h6>Password</h6>
+                        <input type="password" placeholder="• • • • • • • •"></input>
+                    </div>
+
+                <div className="formsubmit">
+                    <button>Submit</button>
+                </div>
+                </div>
+
+            </div>
         </div>
     )
 }
--- frontend/src/Components/Admin/Accounts/Main/AccountsTable.js
+++ frontend/src/Components/Admin/Accounts/Main/AccountsTable.js
@@ -3,7 +3,7 @@ import React from 'react';
 function AccountsTable() {
     return (
         <div className="accounts-table">
-            Table
+            
         </div>
     )
 }
