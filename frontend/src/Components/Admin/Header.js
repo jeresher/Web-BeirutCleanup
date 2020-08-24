9519fb732485eec6101f3835ff8c0432ff492523
Jere Sher <jeresher@g.ucla.edu>
2020-08-23 21:40:02 -0700

    added ability to log out

--- frontend/src/Components/Admin/Header.js
+++ frontend/src/Components/Admin/Header.js
@@ -1,14 +1,25 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import logoutsvg from "../../Assets/icons/logout.svg"
 
 function Header() {
+
+    const history = useHistory();
     
+    function onLogoutClicked() {
+        localStorage.clear();
+        history.push('/admin/')
+    }
 
     return (
         <div className="admin-dashboard-header">
             <div>
                 <h1>Beirut Cleanup</h1>
             </div>
-            <div>
+
+            <div className="exit" onClick={onLogoutClicked}>
+                <h6>Logout </h6>
+                <img src={logoutsvg} />
             </div>
         </div>
     )
