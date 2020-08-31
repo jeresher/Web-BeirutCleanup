950aabe7c2bc07d8c323417438a322649530afe2
Jere Sher <jeresher@g.ucla.edu>
2020-08-30 21:45:26 -0700

    commented out donate button - requested by VolunteerBeirut

--- frontend/src/Components/Main/NavBar.js
+++ frontend/src/Components/Main/NavBar.js
@@ -16,8 +16,8 @@ function NavBar(props) {
           <button id="viewevent" onClick={() => props.loadCreatePage(false)}><h1>VIEW EVENTS</h1><h1>عرض الأحداث</h1></button> :
           <button id="addevent" onClick={() => props.loadCreatePage(true)}><h1>ADD EVENT</h1><h1>إضافة حدث</h1></button>
           }
-        */}
           <a href="https://supportlrc.app/" target="_blank"><button id="donate"><h1>DONATE</h1><h1>تبرع</h1></button></a>
+        */}
         </div>
       </div>
     );
